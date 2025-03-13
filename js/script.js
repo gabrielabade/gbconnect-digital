555f70d5deb0d1d260fcebbe19671425777fbde7
gabrielabade <gabriela.bade@hotmail.com>
2025-03-13 16:29:15 -0300

    adiciona suporte ao tema claro com melhorias de estilo e responsividade

--- js/script.js
+++ js/script.js
@@ -26,7 +26,7 @@ window.onscroll = () => {
       navLinks.forEach(link => {
         link.classList.remove('active');
       });
-      document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      document.querySelector('header nav a[href*=' + id + ']')?.classList.add('active');
     }
   });
 
